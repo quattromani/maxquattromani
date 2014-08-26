620dc24c3c0e839b204a03c500a5884b7bfc6f27
quattromani <max@maxquattromani.com>
2014-08-26 16:36:01 -0600

    Pro Tip: don't take down your site when launching

--- js/build/production.js
+++ js/build/production.js
@@ -2,29 +2,6 @@ var smallBreakPoint = 640;
 var mediumBreakPoint = 768;
 
 /* ==========================================================================
-    Prototype Tools -- Version: 1.9.0.2 - Updated: 1/20/2014
-   ========================================================================== */
-
-// For DEMO site only - DO NOT EVER INGEST THESE !!
-window.onload = getPageLoadTime;
-
-// Calculate the viewport size on prototype site
-$(window).resize(function() {
-    $('.viewport').empty().append($(window).width(), "x", $(window).height());
-}).resize();
-
-//calculate the time before calling the function in window.onload
-var beforeload = (new Date()).getTime();
-
-function getPageLoadTime() {
-    //calculate the current time in afterload
-    var afterload = (new Date()).getTime();
-    // now use the beforeload and afterload to calculate the seconds
-    var seconds = (afterload - beforeload) / 1000;
-    // Place the seconds in the innerHTML to show the results
-    $('.loadtime').text( + seconds + ' sec');
-}
-/* ==========================================================================
     Styleguide -- Version: 0.4.1 - Updated: 2/22/2014
     ========================================================================== */
 
@@ -103,19 +80,6 @@ $(function() {
   $('li:first-child').addClass('js-first');
   $('li:last-child').addClass('js-last');
 });
-
-// Set year
-(function($) {
-
-  $.fn.getYear = function() {
-    var d = new Date();
-    var x = document.getElementById("year");
-    x.innerHTML=d.getFullYear();
-  }
-
-}(jQuery));
-
-$('.getYear').getYear();
 
 /* ==========================================================================
     Modal -- Version: 1.9.0.0 - Updated: 4/28/2014
